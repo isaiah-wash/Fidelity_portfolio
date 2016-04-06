947ba395762b960f5b59c8aa4b5d35e609fbce20
Isaiah Washington <isaiahwashington@isaiahs-mbp.cate.org>
2016-04-05 23:17:06 -0700

    second commit

--- INGN_stock_until3:31:16.py
+++ INGN_stock_until3:31:16.py
@@ -5,21 +5,26 @@ workbook = xlrd.open_workbook(file_location)
 sheet = workbook.sheet_by_index(1)
 
 
-'''if sheet.cell(1,1).ctype == 3:
-	ms_date_number = sheet.cell_value(1,1)
-	ms_date_number = sheet.cell(1,1).cell_value
-
-	year, month, day'''
 
 date_list = []
+date_list2 = []
 stock_price = []
 
 for row in range(sheet.nrows):
-	date_list.append(xlrd.xldate_as_tuple(sheet.cell_value(row,0),0))
-#	date_list.append(sheet.cell_value(row,0))
+	
+	date_list.append(sheet.cell_value(row,0))
 	stock_price.append(sheet.cell_value(row, 1))
-print date_list
-#print xlrd.xldate_as_tuple(sheet.cell_value(row,0),0)
+	date_list2.append(xlrd.xldate_as_tuple(sheet.cell_value(row,0),0))
+	
+
+date_string_list = []
+for i in date_list2:
+
+	date_string_list.append('%g/%g/%g' %(i[0], i[1], i[2]))
+
+month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
 	
 	
 
@@ -28,7 +33,7 @@ plt.plot(date_list, stock_price, color = "blue")
 plt.xlabel('Date')
 plt.ylabel('Stock Price')
 plt.title('1 year INGN Stock')
-#plt.show()
-
+plt.xticks(date_list, date_string_list)
+plt.show()
 
 

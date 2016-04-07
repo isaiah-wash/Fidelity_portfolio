7c3dc91381888bde964b813368523d11194f5494
Isaiah Washington <isaiahwashington@isaiahs-mbp.cate.org>
2016-04-07 08:51:23 -0700

    third commit

--- INGN_stock_until3:31:16.py
+++ INGN_stock_until3:31:16.py
@@ -22,9 +22,9 @@ for i in date_list2:
 
 	date_string_list.append('%g/%g/%g' %(i[0], i[1], i[2]))
 
-month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month_list = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar']
 
-
+graph_list = [date_list[0], date_list[21], date_list[41], date_list[63], date_list[85], date_list[106], date_list[127], date_list[149], date_list[169], date_list[191], date_list[210], date_list[230]]
 	
 	
 
@@ -33,7 +33,8 @@ plt.plot(date_list, stock_price, color = "blue")
 plt.xlabel('Date')
 plt.ylabel('Stock Price')
 plt.title('1 year INGN Stock')
-plt.xticks(date_list, date_string_list)
+plt.xticks(graph_list, month_list)
+#plt.xticks(date_list, date_string_list)
 plt.show()
 
 
